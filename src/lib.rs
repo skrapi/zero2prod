b91478bc968c21b7fae39c0e1b903ab483b4a4db
Sylvan <sylvan@hey.com>
2022-06-01 23:42:53 +0200

    added healt_check_works test

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::dev::Server;
+use std::net::TcpListener;
 
 /// Check that the service is still up
 #[get("/health_check")]
@@ -6,9 +8,10 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(health_check))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().service(health_check))
+        .listen(listener)?
+        .run();
+
+    Ok(server)
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,29 @@
+//! tests/health_check.rs
+use std::net::TcpListener;
+
+#[tokio::test]
+async fn health_check_works() {
+    // This spawns the app, and will drop and destroy it when the test completes
+    let address = spawn_app();
+    
+    let client = reqwest::Client::new();
+
+    let response = client.get(&format!("{}/health_check", &address)).send().await.expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+/// Lauch app
+fn spawn_app() -> String {
+    // port 0 tells the OS to scan for an available port
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
+    let port = listener.local_addr().unwrap().port();
+    
+    let server = zero2prod::run(listener).expect("Failed to bind address.");
+    // spawn a new thread to run the server
+    let _ = tokio::spawn(server);
+
+    // Return address so that the caller can find the app
+    format!("http://127.0.0.1:{}", port)
+}
