5d4f1ff3e7334f46a75fabc595d9c3aae03f9b3b
Sylvan Smit <sjsmit@amazon.com>
2022-06-10 16:44:26 +0200

    clean up instrumentation around subscribe

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,7 +2,6 @@ use actix_web::{post, web, HttpResponse};
 use sqlx::PgPool;
 
 use chrono::Utc;
-use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -12,38 +11,41 @@ struct SubscribeForm {
 }
 
 /// Handle new subscriptions
+/// % tells tracing to use `Display` implementation in logging
+#[tracing::instrument (
+    name = "Adding a new subscriber.",
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
 #[post("/subscriptions")]
-async fn subscribe(form: web::Form<SubscribeForm>, db_pool: web::Data<PgPool>) -> HttpResponse {
-    let request_id = Uuid::new_v4();
+async fn subscribe(form: web::Form<SubscribeForm>, pool: web::Data<PgPool>) -> HttpResponse {
+    match insert_subscriber(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
 
-    // % tells tracing to use `Display` implementation in logging
-    let request_span = tracing::info_span!("Adding a new subscriber.", %request_id, subscriber_email = %form.email, subscriber_name= %form.name);
-    let _request_span_guard = request_span.enter();
-
-    // We do not call `.enter` on query_span!
-    // `.instrument` takes care of it at the right moments
-    // in the quirey future lifetime
-    let query_span = tracing::info_span!("Saving new subscriber details in the database.");
-
-    match sqlx::query!(
+#[tracing::instrument(
+    name = " Saving new subscriber details in the database.",
+    skip(form, pool)
+)]
+async fn insert_subscriber(pool: &PgPool, form: &SubscribeForm) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
         Uuid::new_v4(),
         form.email,
         form.name,
         Utc::now()
     )
-    // We use `get_ref` to get an immutable reference to the `PgPool`
-    // wrapped by `web::Data`.
-    .execute(db_pool.get_ref())
-    .instrument(query_span)
+    .execute(pool)
     .await
-    {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => {
-            // This error log falls outside of the `query_span`
-            // We will rectify that later
-            tracing::error!("Failed to execute query: {:?}", e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
 }
